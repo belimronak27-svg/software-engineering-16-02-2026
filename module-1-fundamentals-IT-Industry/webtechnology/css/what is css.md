9ed0f08a3bf872fa3fdf01e6fb12f250152f03d8
Ronak belim <belimronak27@gmail.com>
2026-02-26 08:09:58 +0530

    26-02-2026 all data uploded

---
+++ module-1-fundamentals-IT-Industry/webtechnology/css/what is css.md
@@ -0,0 +1,10 @@
+# what is css ?
+  css stands for cascading stylesheet 
+  css provides style on your webpage 
+  css extension .css 
+
+# types of css ?
+  
+  1. inline css
+  2. internal css
+  3. external css 
---
+++ module-1-fundamentals-IT-Industry/webtechnology/javascript/calc.js
@@ -0,0 +1,27 @@
+function add(){
+    var a = parseInt(prompt('Enter first value:'));
+    var b = parseInt(prompt('Enter second value:'));
+    var c = a+b;
+    alert(a+'+'+b+'='+c);
+}
+
+function multi(){
+    var a = parseInt(prompt('Enter first value:'));
+    var b = parseInt(prompt('Enter second value:'));
+    var c = a*b;
+    alert(a+'x'+b+'='+c);
+}
+
+function sub(){
+    var a = parseInt(prompt('Enter first value:'));
+    var b = parseInt(prompt('Enter second value:'));
+    var c = a-b;
+    alert(a+'-'+b+'='+c);
+}
+
+function dv(){
+    var a = parseInt(prompt('Enter first value:'));
+    var b = parseInt(prompt('Enter second value:'));
+    var c = a/b;
+    alert(a+'÷'+b+'='+c);
+}
